246c18fc2077148553143807d826819dc2181223
lsk751109-afk <lsk751109@gmail.com>
2026-08-03 20:29:31 +0900

    Add PortOne production payment identifiers

---
+++ assets/js/payment-config.js
@@ -0,0 +1,14 @@
+(() => {
+  'use strict';
+
+  // PortOne V2 browser-side public identifiers.
+  // Do not place PORTONE_API_SECRET or KCP private keys in this file.
+  window.ONGIL_PAYMENT_CONFIG = Object.freeze({
+    storeId: 'store-589361c1-846a-4e5f-be27-177b1a6c2436',
+    channelKey: 'channel-key-88930f08-57d2-4a10-8322-e046a404b065',
+    currency: 'CURRENCY_KRW',
+    payMethod: 'CARD',
+    apiBaseUrl: '',
+    enabled: false
+  });
+})();
